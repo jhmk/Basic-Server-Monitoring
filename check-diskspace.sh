d8065bba6dd80f05c5b412786ce067a171573ceb
jhmk <jhmk@users.noreply.github.com>
2019-07-05 10:45:05 +0200

    Update check-diskspace.sh

--- check-diskspace.sh
+++ check-diskspace.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 disk="$(df -h | grep /dev/xvda1 | awk {'print $5'} | cut -d'%' -f1)"
+localip="$(ifconfig | grep -A1 'enp3s0' | grep inet | awk {'print $2'} )"
 dt="$(date --iso-8601=seconds)"
 echo $disk
 if [ $disk -gt 70 ]; then
 echo "Running out of space \" xvda1 ($disk%)\" on SERVER  as on $(date)"
-curl -s --user 'api:key-XXXXXXX' https://api.mailgun.net/v3/mailgun.XXX/messages -F from='Server Alert <mailgun@mailgun.XXX>' -F to=MAIL -F subject='Server Disk Alert' -F text='Running out of space  xvda1 '$disk' % on SERVER  as on '$dt' '
+curl -s --user 'api:key-XXXXXXX' https://api.mailgun.net/v3/mailgun.XXX/messages -F from='Server Alert <mailgun@mailgun.XXX>' -F to=MAIL -F subject='Server Disk Alert' -F text='Running out of space  xvda1 '$disk' % on SERVER  as on '$dt' with '$localip' '
 fi
